9e9dcf1e2c31346c5f6fb3674840d3d1ac974d37
sis <sisinka3568@gmail.com>
2023-12-27 18:41:27 +0200

    refractured the code

--- diabetes_app.py
+++ diabetes_app.py
@@ -8,32 +8,47 @@
 
 import tkinter as tk
 from tkinter import *
-import joblib
 from tkinter import messagebox
+from joblib import load
+import os
+import pandas as pd
 
-diabetes_prediction_Model = joblib.load('diabetes_prediction_Model.pkl')
-#import os 
+os.chdir('C:\\Users\\15105\\Python\\DiabetesApp\\env\\src')
+diabetes_prediction_Model = load('model_.pkl')
+
+#import os
 #print(os.getcwd())
 
 def analyze():
-    # get the values from the entries
-    preg = float(entry_pregnancies.get())
-    gluco = float(entry_glucose.get())
-    bp = float(entry_bloodpressure.get())
-    stinmm = float(entry_skinthickness.get())
-    insulin = float(entry_insulin.get())
-    mass = float(entry_bmi.get())
-    dpf = float(entry_diabetesfunction.get())
-    age = float(entry_age.get())
-    # create a 2D array with the values
-    values = [[preg, gluco, bp, stinmm, insulin, mass, dpf, age]]
-    # model prediction
-    prediction = diabetes_prediction_Model.predict(values)
+    """Saves as a csv file and predicts if the person has diabetes or not."""
+    # gets the values from input entries
+    values = get_values()
+
+    # create a dataframe with the values
+    df = pd.DataFrame([values], columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age'])
+    
+    # make a prediction
+    prediction = diabetes_prediction_Model.predict(df)
     if prediction[0] == 1:
         messagebox.showinfo("Prediction", "The model predicts: Diabetes ")
     else:
         messagebox.showinfo("Prediction", "The model predicts: No Diabetes")
-   
+
+
+def get_values():
+    """Gets the values from input entries."""
+    values = {
+    'Pregnancies': float(entry_pregnancies.get()),
+    'Glucose': float(entry_glucose.get()),
+    'BloodPressure': float(entry_bloodpressure.get()),
+    'SkinThickness': float(entry_skinthickness.get()),
+    'Insulin': float(entry_insulin.get()),
+    'BMI': float(entry_bmi.get()),
+    'DiabetesPedigreeFunction': float(entry_diabetesfunction.get()),
+    'Age': float(entry_age.get())
+    }
+    return values
+
 
 root = tk.Tk()
 root.title("Diabetes Prediction App")
@@ -41,63 +56,82 @@ root.geometry("600x600")
 root.configure(bg='light blue')
 
 # This is a label widget
-label0 = tk.Label(root, text="Does this person have diabetes?", bg='light blue', font=('arial', 14, 'bold'))
-label0.grid(row=0, column=1)  # This will place label0 at position (0,0)
+label0 = tk.Label(root,
+                  text="Does this person have diabetes?",
+                  bg='light blue',
+                  font=('arial', 14, 'bold')
+)
+# This will place label0 at position (0,0)
+label0.grid(row=0, column=1)
 
 label_pregnancies = tk.Label(root, text="Pregnancies", bg='light blue')
-label_pregnancies.grid(row=1, column=0)  # This will place label_pregnancies at position (0,0)
+# This will place label_pregnancies at position (0,0)
+label_pregnancies.grid(row=1, column=0)
 
 entry_pregnancies = tk.Entry(root)
-entry_pregnancies.grid(row=1, column=1)  # This will place entry_pregnancies at position (0,1)
+# This will place entry_pregnancies at position (0,1)
+entry_pregnancies.grid(row=1, column=1)
 
 label_glucose = tk.Label(root, text="Glucose", bg="light blue")
-label_glucose.grid(row=2, column=0)  # This will place label_glucose at position (1,0)
+# This will place label_glucose at position (1,0)
+label_glucose.grid(row=2, column=0)
 
 entry_glucose = tk.Entry(root)
-entry_glucose.grid(row=2, column=1)  # This will place entry_glucose at position (1,1)
+# This will place entry_glucose at position (1,1)
+entry_glucose.grid(row=2, column=1)
 
 label_bloodpressure = tk.Label(root, text="BloodPressure", bg="light blue")
-label_bloodpressure.grid(row=3, column=0)  # This will place label_bloodpressure at position (2,0)
+# This will place label_bloodpressure at position (2,0)
+label_bloodpressure.grid(row=3, column=0)
 
 entry_bloodpressure = tk.Entry(root)
-entry_bloodpressure.grid(row=3, column=1)  # This will place entry_bloodpressure at position (2,1)
-
+# This will place entry_bloodpressure at position (2,1)
+entry_bloodpressure.grid(row=3, column=1)
 label_skinthickness = tk.Label(root, text="SkinThickness", bg="light blue")
-label_skinthickness.grid(row=4, column=0)  # This will place label_skinthickness at position (3,0)
+# This will place label_skinthickness at position (3,0)
+label_skinthickness.grid(row=4, column=0)
 
 entry_skinthickness = tk.Entry(root)
-entry_skinthickness.grid(row=4, column=1)  # This will place entry_skinthickness at position (3,1)
+# This will place entry_skinthickness at position (3,1)
+entry_skinthickness.grid(row=4, column=1)
 
+# This will place label_insulin at position (4,0)
 label_insulin = tk.Label(root, text="Insulin", bg="light blue")
-label_insulin.grid(row=5, column=0)  # This will place label_insulin at position (4,0)
+label_insulin.grid(row=5, column=0)
 
+# This will place entry_insulin at position (4,1)
 entry_insulin = tk.Entry(root)
-entry_insulin.grid(row=5, column=1)  # This will place entry_insulin at position (4,1)
+entry_insulin.grid(row=5, column=1)
 
+# This will place label_bmi at position (5,0)
 label_bmi = tk.Label(root, text="BMI", bg="light blue")
-label_bmi.grid(row=6, column=0)  # This will place label_bmi at position (5,0)
+label_bmi.grid(row=6, column=0)
 
+# This will place entry_diabetesfunction at position (6,1)
 entry_bmi = tk.Entry(root)
-entry_bmi.grid(row=6, column=1)  # This will place entry_bmi at position (5,1)
+entry_bmi.grid(row=6, column=1)
 
 label_diabetesfunction = tk.Label(root, text="DiabetesPedigreeFunction", bg="light blue")
-label_diabetesfunction.grid(row=7, column=0)  # This will place label_diabetesfunction at position (6,0)
 
+# This will place label_diabetesfunction at position (6,0)
+label_diabetesfunction.grid(row=7, column=0)
+
+# This will place entry_diabetesfunction at position (6,1)
 entry_diabetesfunction = tk.Entry(root)
-entry_diabetesfunction.grid(row=7, column=1)  # This will place entry_diabetesfunction at position (6,1)
+entry_diabetesfunction.grid(row=7, column=1)
 
 label_age = tk.Label(root, text="Age", bg="light blue")
-label_age.grid(row=9, column=0)  # This will place label_age at position (7,0)
+# This will place label_age at position (7,0)
+label_age.grid(row=9, column=0)
 
 entry_age = tk.Entry(root)
-entry_age.grid(row=9, column=1)  # This will place entry_age at position (7,1)
+# This will place entry_age at position (7,1)
+entry_age.grid(row=9, column=1)
 
 # This is a button to Analyze the data
 button1 = tk.Button(root, text="Analyze", bg="blue", foreground="white", command=analyze)
-button1.grid(row=10, column=1)  # This will place button1 at position (8,1)
+# This will place button1 at position (8,1)
+button1.grid(row=10, column=1)
 
 root.mainloop()
-
-
-
 
